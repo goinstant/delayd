25cf0c28d4fab16baab6825a8ca668b55d54b92a
James Bowes <jbowes@repl.ca>
2014-05-28 12:05:11 -0300

    Read initial send time from db, and reset after send

--- storage.go
+++ storage.go
@@ -41,21 +41,34 @@ func NewStorage(send AmqpSender) (s *Storage, err error) {
 		return
 	}
 
-	s.timerRunning = true
-	s.timer = time.NewTimer(time.Duration(3) * time.Second)
-	s.nextSend = time.Now().Add(time.Duration(3) * time.Second)
-	go func() {
-		// XXX change the timer to run a SEND command through raft, and emit on applying that command.
-		t := <-s.timer.C
-		entries, err := s.get(t)
-		// XXX cleanup and shut down properly
-		if err != nil {
-			log.Fatal("Could not read entries from db: ", err)
-		}
+	t, err := s.nextTime()
+	if err != nil {
+		return
+	}
 
-		log.Printf("Sending %d entries\n", len(entries))
-		for _, e := range entries {
-			s.send.C <- e
+	s.timer = time.NewTimer(time.Duration(24) * time.Hour)
+	s.resetTimer(t)
+
+	go func() {
+		for {
+			// XXX change the timer to run a SEND command through raft, and emit on applying that command.
+			t := <-s.timer.C
+			entries, err := s.get(t)
+			// XXX cleanup and shut down properly
+			if err != nil {
+				log.Fatal("Could not read entries from db: ", err)
+			}
+
+			log.Printf("Sending %d entries\n", len(entries))
+			for _, e := range entries {
+				s.send.C <- e
+			}
+
+			t, err = s.nextTime()
+			if err != nil {
+				log.Fatal("Could not read next time from db: ", err)
+			}
+			s.resetTimer(t)
 		}
 	}()
 
@@ -84,9 +97,7 @@ func (s *Storage) Add(e Entry, r []byte) error {
 	txn.Put(dbi, k, v, 0)
 
 	if !s.timerRunning || e.SendAt.Before(s.nextSend) {
-		s.timer.Reset(time.Now().Sub(e.SendAt))
-		s.nextSend = e.SendAt
-		s.timerRunning = true
+		s.resetTimer(e.SendAt)
 	}
 
 	txn.Commit()
@@ -144,6 +155,50 @@ func (s *Storage) get(t time.Time) (entries []Entry, err error) {
 	}
 
 	return
+}
+
+// get the next entry send time from the db
+func (s *Storage) nextTime() (t time.Time, err error) {
+	txn, err := s.env.BeginTxn(nil, mdb.RDONLY)
+	if err != nil {
+		log.Println("Error creating transaction: ", err)
+		return
+	}
+	defer txn.Abort()
+
+	dbi, err := txn.DBIOpen(nil, 0)
+	if err != nil {
+		log.Println("Error opening dbi: ", err)
+		return
+	}
+	defer s.env.DBIClose(dbi)
+
+	cursor, err := txn.CursorOpen(dbi)
+	if err != nil {
+		log.Println("Error getting cursor: ", err)
+		return
+	}
+	defer cursor.Close()
+
+	k, _, err := cursor.Get(nil, mdb.FIRST)
+	if err != nil {
+		log.Println("Error reading next time from db: ", err)
+		return
+	}
+
+	t = time.Unix(0, int64(bytesToUint64(k)))
+
+	return
+}
+
+func (s *Storage) resetTimer(t time.Time) {
+	if t.Before(time.Now()) {
+		t = time.Now()
+	}
+	log.Println("Setting next timer for: ", t)
+	s.timerRunning = true
+	s.timer.Reset(t.Sub(time.Now()))
+	s.nextSend = t
 }
 
 // Converts bytes to an integer
